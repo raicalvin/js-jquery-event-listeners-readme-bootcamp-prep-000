452173f6431d45fae56e090e86fd0ce53697caa0
Calvin Simran Rai <raicalvin@gmail.com>
2018-08-23 19:54:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,14 +1,16 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
 $(document).ready(function(){
 
   // call functions here
+  getIt();
 
-  function getIt() {
-    $('p').on('click', function() {
-      alert("Hey!");
-    });
-  }
 
 
 });
