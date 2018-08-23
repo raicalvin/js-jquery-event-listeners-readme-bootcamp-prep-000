852f598e1e80da34b9c3f3313b527b8b518dfa2e
Calvin Simran Rai <raicalvin@gmail.com>
2018-08-23 20:05:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,9 +13,9 @@ function frameIt() {
 }
 
 function pressIt() {
-  $(document).on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if (key.which == 71) {
-      alert()
+      alert("You pressed G!")
     }
   });
 }
